2bb8049ee839b8eb65437791256b152042969068
Vikhyat Agrawal <82029380+vantablack0@users.noreply.github.com>
2022-02-10 16:45:13 +0530

    Create color_jitter.py

---
+++ color_jitter.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+from numpy import random
+
+def colorjitter(img, cj_type="b"):
+    '''
+    ### Different Color Jitter ###
+    img: image
+    cj_type: {b: brightness, s: saturation, c: constast}
+    '''
+    if cj_type == "b":
+        # value = random.randint(-50, 50)
+        value = np.random.choice(np.array([-50, -40, -30, 30, 40, 50]))
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(hsv)
+        if value >= 0:
+            lim = 255 - value
+            v[v > lim] = 255
+            v[v <= lim] += value
+        else:
+            lim = np.absolute(value)
+            v[v < lim] = 0
+            v[v >= lim] -= np.absolute(value)
+
+        final_hsv = cv2.merge((h, s, v))
+        img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+        return img
+    
+    elif cj_type == "s":
+        # value = random.randint(-50, 50)
+        value = np.random.choice(np.array([-50, -40, -30, 30, 40, 50]))
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(hsv)
+        if value >= 0:
+            lim = 255 - value
+            s[s > lim] = 255
+            s[s <= lim] += value
+        else:
+            lim = np.absolute(value)
+            s[s < lim] = 0
+            s[s >= lim] -= np.absolute(value)
+
+        final_hsv = cv2.merge((h, s, v))
+        img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+        return img
+    
+    elif cj_type == "c":
+        brightness = 10
+        contrast = random.randint(40, 100)
+        dummy = np.int16(img)
+        dummy = dummy * (contrast/127+1) - contrast + brightness
+        dummy = np.clip(dummy, 0, 255)
+        img = np.uint8(dummy)
+        return img
